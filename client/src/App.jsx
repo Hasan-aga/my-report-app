1a4fa4613115b94266a45b65ba053370a786ddf0
hasan aga <careerforma@gmail.com>
2025-02-19 16:00:39 +0300

    change color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,18 @@ import ReportGeneratorPage from './components/ReportGeneratorPage';
 
 const theme = createTheme({
   palette: {
+    type: 'light',
     primary: {
-      main: '#1976d2',
+      main: '#283618',
+      light: '#606C38',
+      contrastText: '#fefae0',
     },
     secondary: {
-      main: '#dc004e',
+      main: '#f50057',
+    },
+    background: {
+      default: '#606c38',
+      paper: '#acb785',
     },
   },
 });
@@ -21,6 +28,7 @@ function App() {
         height: '100vh',
         margin: 0,
         padding: 0,
+        backgroundColor: 'background.default'
       }}>
         <ReportGeneratorPage />
       </Box>
