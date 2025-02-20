20462ded3d82b22304f52648d6edc891ece8bc2b
hasan aga <careerforma@gmail.com>
2025-02-20 21:09:58 +0300

    center vertically

--- client/src/components/ReportGeneratorPage.jsx
+++ client/src/components/ReportGeneratorPage.jsx
@@ -19,68 +19,75 @@ const ReportGeneratorPage = () => {
   const theme = useTheme()
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2} sx={{ width: "100%" }}>
-        {/* Categories Panel */}
-
-        <Grid size={2}>
-          <Paper elevation={2} sx={{ p: 3, height: "90vh" }}>
-            <FormControl component="fieldset">
-              <FormLabel
-                component="legend"
-                sx={{
-                  fontSize: "1.2rem",
-                  mb: 2,
-                  color: theme.palette.primary.contrastText
-                }}
-              >
-                Report Categories
-              </FormLabel>
-              <RadioGroup
-                value={selectedCategory}
-                onChange={(e) => setSelectedCategory(e.target.value)}
-              >
-                {Object.entries(REPORT_DATA).map(([key, category]) => (
-                  <FormControlLabel
-                    key={key}
-                    value={key}
-                    control={
-                      <Radio
-                        sx={{
-                          color: theme.palette.secondary.main,
-                          "&.Mui-checked": {
-                            color: theme.palette.secondary.main
-                          }
-                        }}
-                      />
-                    }
-                    label={
-                      <Typography variant="subtitle1">
-                        {category.name}
-                      </Typography>
-                    }
-                    sx={{
-                      mb: 2,
-                      "&:hover": {
-                        backgroundColor: "action.hover",
-                        borderRadius: 1
+    <Box
+      sx={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} sx={{ width: "100%" }}>
+          {/* Categories Panel */}
+          <Grid size={2}>
+            <Paper elevation={2} sx={{ p: 3, height: "90vh" }}>
+              <FormControl component="fieldset">
+                <FormLabel
+                  component="legend"
+                  sx={{
+                    fontSize: "1.2rem",
+                    mb: 2,
+                    color: theme.palette.primary.contrastText
+                  }}
+                >
+                  Report Categories
+                </FormLabel>
+                <RadioGroup
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                >
+                  {Object.entries(REPORT_DATA).map(([key, category]) => (
+                    <FormControlLabel
+                      key={key}
+                      value={key}
+                      control={
+                        <Radio
+                          sx={{
+                            color: theme.palette.secondary.main,
+                            "&.Mui-checked": {
+                              color: theme.palette.secondary.main
+                            }
+                          }}
+                        />
                       }
-                    }}
-                  />
-                ))}
-              </RadioGroup>
-            </FormControl>
-          </Paper>
+                      label={
+                        <Typography variant="subtitle1">
+                          {category.name}
+                        </Typography>
+                      }
+                      sx={{
+                        mb: 2,
+                        "&:hover": {
+                          backgroundColor: "action.hover",
+                          borderRadius: 1
+                        }
+                      }}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </Paper>
+          </Grid>
+          {/* Findings Panel */}
+          <Grid size={10}>
+            <DataSelectionPage
+              category={selectedCategory}
+              templatePath={PDF_TEMPLATE_PATH}
+            />
+          </Grid>
         </Grid>
-
-        {/* Findings Panel */}
-        <Grid size={10}>
-          <DataSelectionPage
-            category={selectedCategory}
-            templatePath={PDF_TEMPLATE_PATH}
-          />
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   )
 }
