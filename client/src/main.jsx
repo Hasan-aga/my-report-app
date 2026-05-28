eac839e293c46b9976f1ac2a380db50c9d1ea528
hasan aga <careerforma@gmail.com>
2026-05-29 00:25:13 +0300

    perf: remove unused react-router-dom from initial bundle
    BrowserRouter was loaded but no routes were defined in the active
    app. Removing it reduces the initial JavaScript bundle.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
 import { SettingsProvider } from "../hooks/useSettings.jsx"
 import App from "./App.jsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <SettingsProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </SettingsProvider>
-  </BrowserRouter>
+  <SettingsProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </SettingsProvider>
 )
